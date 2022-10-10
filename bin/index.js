cbc6a7775a757fb62e10ea4a2122a8aa3e882f82
arun prajapati <arunkp1122@gmail.com>
2022-10-10 20:59:50 +0545

    Fixed Typo

--- bin/index.js
+++ bin/index.js
@@ -8,7 +8,9 @@ const runCommand = (command) => {
       execSync(`${command}`, { stdio: 'inherit' })
    } catch (e) {
       console.error(`Failed to execute ${command}`, e)
+      return false
    }
+   return true
 }
 
 const repoName = process.argv[2]
@@ -17,23 +19,24 @@ const cloneRepo = `git clone --depth 1 --branch next-starter https://github.com/
 
 const c = `cd ${repoName}`
 
-const cloned = runCommand(cloneRepo)
+console.log('Clonning...')
 
+const cloned = runCommand(cloneRepo)
 if (!cloned) {
    console.error(`Failed to clone https://github.com/arun-praj/packages.git`)
    process.exit(-1)
 }
 
-console.log(`Installing dependencies ${repoName}`)
-runCommand(`cd ${reponame}`)
+console.log(`Installing dependencies...`)
+runCommand(`cd ${repoName}`)
 const installed = runCommand(installDependencyCmd)
 
 if (!installed) {
    console.error(`Failed to install npm dependencies`)
    process.exit(-1)
 }
-
-console.log('Completed setup')
+console.log('\n\n')
+console.log('Setup complete !!!')
 console.log('\n\n')
 
 console.log(`1. cd ${repoName} \n2. npm run dev`)
